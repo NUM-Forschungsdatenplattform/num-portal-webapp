190f873a15e5fd32e1cc25d646de3d000479b75c
Sommer <sommekim@mh-hannover.local>
2020-10-02 13:56:35 +0200

    added @angular/flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatSliderModule,
   ],
   providers: [],
