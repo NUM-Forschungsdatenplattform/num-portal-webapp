e00f5b2c2d13af0d947ef215360e92c76e8b2c1c
kksommer <64401294+kksommer@users.noreply.github.com>
2020-10-02 16:30:52 +0200

    Feature/num 119 angular material setup (#1)
    * setup angular material
    
    * added @angular/flex-layout
    
    Co-authored-by: Sommer <sommekim@mh-hannover.local>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
