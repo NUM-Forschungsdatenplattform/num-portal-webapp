b855dd20905fdaafe66e2907acdc620583e34da5
Lars Schäfer <lars.schaefer@appsfactory.de>
2020-10-05 21:06:16 +0200

    feat(NUM-132) Changes component-prefix from app to num

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'num-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
