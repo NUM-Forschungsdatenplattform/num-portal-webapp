4b6d9a5ee366f031465b7c9b796bfea5be522095
Sommer <sommekim@mh-hannover.local>
2020-10-07 12:09:53 +0200

    Adds ngx-translate and http-loader, adds language component, adds i18n (de.json + en.json)

---
+++ src/app/layout/components/language/language.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'num-language',
+  templateUrl: './language.component.html',
+  styleUrls: ['./language.component.scss']
+})
+export class LanguageComponent implements OnInit {
+
+constructor(public translate: TranslateService) {
+    translate.addLangs(['de', 'en']);
+    translate.setDefaultLang('de');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/de|en/) ? browserLang : 'de');
+  }
+
+  ngOnInit(): void {
+  }
+
+  switchLanguage(): void {
+    this.translate.use(this.translate.currentLang === 'de' ? 'en' : 'de');
+  }
+
+}
