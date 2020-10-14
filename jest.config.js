b0cdeda32f0fe1a63ed3d4f4c032bc94ab6372b4
Lars Schäfer <lars.schaefer@appsfactory.de>
2020-10-14 11:05:07 +0200

    feat(NUM-266): Adds Jest

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
+
+module.exports = {
+  preset: "jest-preset-angular",
+  roots: ["<rootDir>/src/"],
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  setupFilesAfterEnv: ["<rootDir>/src/test.ts"],
+  collectCoverage: true,
+  coverageReporters: [
+    "html",
+    "text-summary",
+    "json",
+    "lcov",
+    "text",
+    "clover",
+    "cobertura",
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: "<rootDir>/",
+  }),
+};
