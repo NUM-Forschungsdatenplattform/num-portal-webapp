932e427342bd6ebba7be6626e7532af263887862
Lars Schäfer <lars.schaefer@appsfactory.de>
2020-10-29 16:14:51 +0100

    feat(NUM-121): Adds Reactive Form

--- src/app/modules/phenotypes/components/phenotype-editor/phenotype-editor.component.ts
+++ src/app/modules/phenotypes/components/phenotype-editor/phenotype-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
+import { IPhenotypeResolved } from '../../models/phenotype-resolved.interface'
 
 @Component({
   selector: 'num-phenotype-editor',
@@ -6,7 +9,20 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./phenotype-editor.component.scss'],
 })
 export class PhenotypeEditorComponent implements OnInit {
-  constructor() {}
+  resolvedData: IPhenotypeResolved
+  generalInfoForm: FormGroup
 
-  ngOnInit(): void {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.resolvedData = this.route.snapshot.data.resolvedData
+    this.generalInfoForm = new FormGroup({
+      title: new FormControl(this.resolvedData.phenotype?.name),
+      description: new FormControl(this.resolvedData.phenotype?.description),
+    })
+  }
+
+  saveForm(): void {
+    console.log('Save')
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TranslateModule } from '@ngx-translate/core'
 import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
+
+const SHARED_MODULES = [TranslateModule, FormsModule, ReactiveFormsModule]
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule],
-  exports: [TranslateModule, FormsModule],
+  imports: [...SHARED_MODULES, CommonModule],
+  exports: [...SHARED_MODULES],
 })
 export class SharedModule {}
