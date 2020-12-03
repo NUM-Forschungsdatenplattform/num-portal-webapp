180433856e9439aeaaea0ec54d9b985140948fc5
Lars Schäfer <lars.schaefer@appsfactory.de>
2020-12-03 09:54:22 +0100

    feat(NUM-311): Updates study and cohort models

--- src/app/shared/models/study/study-ui.model.ts
+++ src/app/shared/models/study/study-ui.model.ts
@@ -1,19 +1,35 @@
+import { ITemplateMetaData } from '../template-metadata.interface'
 import { CohortGroupUiModel } from './cohort-group-ui.model'
 import { IStudyApi } from './study-api.interface'
+import { StudyStatus } from './study-status.enum'
 
 export class StudyUiModel {
-  id: number | null
-  studyId: number | null
-  name: string
-  description?: string
-  primaryHypothesis = 'Currently not implemented on backend'
-  secondaryHypothesis?: string
   cohortGroup: CohortGroupUiModel
+  /** The coordinator of the study. Is automatically based on the auth-token */
+  coordinator?: any // TODO
+  description?: string
+  firstHypotheses?: string
+  id: number | null
+  modifiedDate?: Date
+  name?: string
+  researchers: any[] // TODO
+  secondHypotheses?: string
+  status: StudyStatus
+  templates: ITemplateMetaData[]
+
   constructor(apiStudy?: IStudyApi) {
-    this.id = apiStudy?.id || null
-    this.studyId = apiStudy?.studyId
-    this.name = apiStudy?.name
+    this.coordinator = apiStudy?.coordinator || undefined
     this.description = apiStudy?.description || undefined
+    this.firstHypotheses = apiStudy?.firstHypotheses || undefined
+    this.id = apiStudy?.id || null
+    this.modifiedDate = apiStudy?.modifiedDate || undefined
+    this.name = apiStudy?.name || undefined
+    this.secondHypotheses = apiStudy?.secondHypotheses || undefined
+    this.status = apiStudy?.status || StudyStatus.Draft
+
     this.cohortGroup = new CohortGroupUiModel()
+
+    // this.researchers =
+    // this.templates =
   }
 }
