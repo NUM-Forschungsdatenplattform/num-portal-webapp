ed7fff8b8b0c51b3d4026622720c356ab795a6b4
Lars Schäfer <lars.schaefer@appsfactory.de>
2020-12-03 11:27:13 +0100

    feat(NUM-311): Updates UserDetail Model

--- src/app/shared/models/study/study-api.interface.ts
+++ src/app/shared/models/study/study-api.interface.ts
@@ -1,17 +1,17 @@
+import { IUserDetails } from '../user/user-details.interface'
 import { StudyStatus } from './study-status.enum'
 import { IStudyTemplateInfoApi } from './study-template-info-api.interface'
-import { IStudyUserDetailApi } from './study-user.detail-api.interface'
 
 export interface IStudyApi {
   cohortId?: number
-  coordinator?: IStudyUserDetailApi
+  coordinator?: IUserDetails
   createDate?: Date
   description?: string
   firstHypotheses?: string
   id?: number | null
   modifiedDate?: Date
   name?: string
-  researchers?: IStudyUserDetailApi[]
+  researchers?: IUserDetails[]
   secondHypotheses?: string
   status?: StudyStatus
   templates?: IStudyTemplateInfoApi
