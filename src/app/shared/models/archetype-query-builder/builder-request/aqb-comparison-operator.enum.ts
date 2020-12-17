ee51c1908f5752eb92fa39255ed7db6df76aff72
Lars Schäfer <lars.schaefer@appsfactory.de>
2020-12-17 17:15:47 +0100

    feat(NUM-709): Adds aqb models

---
+++ src/app/shared/models/archetype-query-builder/builder-request/aqb-comparison-operator.enum.ts
@@ -0,0 +1,19 @@
+export enum AqbComparisonOperator {
+  /** Equal to */
+  Eq = 'EQ',
+
+  /** Greater than */
+  Gt = 'GT',
+
+  /** Greater than or equal to */
+  Gt_eq = 'GT_EQ',
+
+  /** Less than */
+  Lt = 'LT',
+
+  /** Less than or equal to */
+  Lt_eq = 'LT_EQ',
+
+  /** Not equal to */
+  Neq = 'NEQ',
+}
