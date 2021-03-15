59036f931f1ca91d25dcdb7a07d4e9d8a55458d0
Mina <mina.moanes@appsfactory.de>
2021-03-15 12:40:21 +0100

    feat(NUM-1236): Add Content Cards

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
 import { Subscription } from 'rxjs'
 import { AuthService } from 'src/app/core/auth/auth.service'
+import { ContentService } from 'src/app/core/services/content/content.service'
 import { AvailableRoles } from 'src/app/shared/models/available-roles.enum'
+import { IDashboardCard } from 'src/app/shared/models/content/dashboard-card.interface'
 import { IAuthUserInfo } from 'src/app/shared/models/user/auth-user-info.interface'
 import { AppConfigService } from '../../../../config/app-config.service'
 import { INITIATIVE_CLINICS_LOGOS, LOGOS_BASE_URL, PARTICIPANT_CLINICS_LOGOS } from './constants'
@@ -15,13 +18,21 @@ export class DashboardComponent implements OnInit, OnDestroy {
   private subscriptions = new Subscription()
   AvailableRoles = AvailableRoles
   user: IAuthUserInfo
-  constructor(private appConfig: AppConfigService, private authService: AuthService) {}
+  constructor(
+    private appConfig: AppConfigService,
+    private authService: AuthService,
+    private contentService: ContentService,
+    private translateService: TranslateService
+  ) {}
 
   config = this.appConfig.config
   participantLogosBaseUrl = LOGOS_BASE_URL
   participantLogos = PARTICIPANT_CLINICS_LOGOS
   initiativeLogos = INITIATIVE_CLINICS_LOGOS
   authTest: string
+  cards: IDashboardCard[]
+  displayLang: 'de' | 'en'
+  isLoggedIn: boolean
 
   @ViewChild('participantsAnchor') participantsAnchor: ElementRef
 
@@ -29,10 +40,45 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.subscriptions.add(
       this.authService.userInfoObservable$.subscribe((userInfo) => this.handleUserInfo(userInfo))
     )
+
+    this.isLoggedIn = this.authService.isLoggedIn
+
+    if (this.isLoggedIn) {
+      this.fetchContentCards()
+      this.getCurrentLang()
+    }
   }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe()
+  }
+
+  fetchContentCards(): void {
+    this.subscriptions.add(
+      this.contentService.getCards().subscribe(
+        (data) => {
+          this.cards = data
+          // this.isLoading = false
+        },
+        () => {
+          // this.isLoading = false
+        }
+      )
+    )
+  }
+
+  openCardUrl(cardUrl: string): void {
+    console.log(cardUrl)
+    window.open(cardUrl)
+  }
+
+  getCurrentLang(): void {
+    this.displayLang = (this.translateService.currentLang as unknown) as 'en' | 'de'
+
+    this.displayLang = this.translateService.currentLang as 'en' | 'de'
+    this.subscriptions.add(
+      this.translateService.onLangChange.subscribe((newLang) => (this.displayLang = newLang.lang))
+    )
   }
 
   handleUserInfo(userInfo: IAuthUserInfo): void {
