a35c88614f17342514034b03d2d4333207c59966
Lars Schäfer <lars.schaefer@appsfactory.de>
2021-07-30 11:44:45 +0200

    feat(NUM-1752): Adds resolving of pseudonyms

---
+++ src/app/core/services/manager/manager.service.spec.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http'
+import { TestBed } from '@angular/core/testing'
+import { of, throwError } from 'rxjs'
+import { AppConfigService } from 'src/app/config/app-config.service'
+
+import { ManagerService } from './manager.service'
+
+describe('ManagerService', () => {
+  let service: ManagerService
+
+  const httpClient = ({
+    get: jest.fn(),
+  } as unknown) as HttpClient
+
+  const appConfig = {
+    config: {
+      api: {
+        baseUrl: 'localhost/api',
+      },
+    },
+  } as AppConfigService
+
+  beforeEach(() => {
+    service = new ManagerService(httpClient, appConfig)
+    jest.spyOn(service, 'handleError')
+    jest.clearAllMocks()
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy()
+  })
+
+  describe('When the pseodonym is supposed to be resolved', () => {
+    const projectId = 1
+    const pseudonym = 'abc123'
+    const resolvedValue = '123abc'
+
+    it('should call the api with the projectId and pseudonym', async () => {
+      jest.spyOn(httpClient, 'get').mockImplementation(() => of(resolvedValue))
+      const result = await service.resolvePseudonym(projectId, pseudonym).toPromise()
+      expect(httpClient.get).toHaveBeenCalledWith(
+        `${appConfig.config.api.baseUrl}/project/${projectId}/resolve/${pseudonym}`
+      )
+      expect(result).toEqual(resolvedValue)
+    })
+
+    it('should call the api and handle erros', async (done) => {
+      jest.spyOn(httpClient, 'get').mockImplementation(() => throwError('Error'))
+
+      try {
+        await service.resolvePseudonym(projectId, pseudonym).toPromise()
+      } catch (error) {
+        expect(httpClient.get).toHaveBeenCalledWith(
+          `${appConfig.config.api.baseUrl}/project/${projectId}/resolve/${pseudonym}`
+        )
+        expect(service.handleError).toHaveBeenCalled()
+        done()
+      }
+    })
+  })
+})
---
+++ src/app/modules/manager-tools/components/pseudonym-resolver/pseudonym-resolver.component.spec.ts
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2021 Vitagroup AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ReactiveFormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { TranslateModule } from '@ngx-translate/core'
+import { of, throwError } from 'rxjs'
+import { ManagerService } from 'src/app/core/services/manager/manager.service'
+import { ToastMessageService } from 'src/app/core/services/toast-message/toast-message.service'
+import { MaterialModule } from 'src/app/layout/material/material.module'
+import { ButtonComponent } from 'src/app/shared/components/button/button.component'
+import { RESOLVE_ERROR_CONFIG } from './constants'
+
+import { PseudonymResolverComponent } from './pseudonym-resolver.component'
+
+describe('PseudonymResolverComponent', () => {
+  let component: PseudonymResolverComponent
+  let fixture: ComponentFixture<PseudonymResolverComponent>
+
+  const mockToastMessageService = ({
+    openToast: jest.fn(),
+  } as unknown) as ToastMessageService
+
+  const mockManagerService = ({
+    resolvePseudonym: jest.fn(),
+  } as unknown) as ManagerService
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PseudonymResolverComponent, ButtonComponent],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        TranslateModule.forRoot(),
+      ],
+      providers: [
+        {
+          provide: ToastMessageService,
+          useValue: mockToastMessageService,
+        },
+        {
+          provide: ManagerService,
+          useValue: mockManagerService,
+        },
+      ],
+    }).compileComponents()
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    fixture = TestBed.createComponent(PseudonymResolverComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+
+  describe('When the user enters the project id and pseudonym to resolve them to a patient id', () => {
+    const pseudonym = '64-Chars-Long-Pseudonym-String-That-Will-Resolve-To-A-Patient-Id'
+    const projectId = '1'
+    beforeEach(() => {
+      const projectIdInput = fixture.nativeElement.querySelector(
+        `[data-test="manager-tools__resolve-pseudonym__projectid-input"]`
+      )
+      const pseudonymInput = fixture.nativeElement.querySelector(
+        `[data-test="manager-tools__resolve-pseudonym__pseudonym-input"]`
+      )
+      projectIdInput.value = projectId
+      projectIdInput.dispatchEvent(new Event('input'))
+
+      pseudonymInput.value = pseudonym
+      pseudonymInput.dispatchEvent(new Event('input'))
+    })
+
+    it('should call the service to resolve and display the returned value', () => {
+      jest.spyOn(mockManagerService, 'resolvePseudonym').mockImplementation(() => of('resolved'))
+      component.resolvePseudonym()
+      fixture.detectChanges()
+      expect(mockManagerService.resolvePseudonym).toHaveBeenCalledWith(projectId, pseudonym)
+
+      const value = fixture.nativeElement.querySelector(
+        `[data-test="manager-tools__resolve-pseudonym__pseudonym-value"]`
+      )
+
+      expect(value.innerHTML.trim()).toEqual('resolved')
+    })
+
+    it('should display the error message if the service fails to resolve', () => {
+      jest
+        .spyOn(mockManagerService, 'resolvePseudonym')
+        .mockImplementation(() => throwError('Error'))
+      component.resolvePseudonym()
+      expect(mockManagerService.resolvePseudonym).toHaveBeenCalledWith(projectId, pseudonym)
+      expect(mockToastMessageService.openToast).toHaveBeenCalledWith(RESOLVE_ERROR_CONFIG)
+    })
+  })
+})
