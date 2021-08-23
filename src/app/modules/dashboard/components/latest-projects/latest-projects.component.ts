7a42dda66ace3075c53430e48d2d3daeaf553f66
Axel Siebert <a.siebert_84@gmx.de>
2021-08-23 13:32:16 +0200

    Rename latest projects component
    - Rename dashboard/projects component to latest-projects-component
    - Add translated headers to latest projects table

--- src/app/modules/dashboard/components/latest-projects/latest-projects.component.ts
+++ src/app/modules/dashboard/components/latest-projects/latest-projects.component.ts
@@ -19,11 +19,11 @@ import { Subscription } from 'rxjs'
 import { ContentService } from 'src/app/core/services/content/content.service'
 
 @Component({
-  selector: 'num-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss'],
+  selector: 'num-latest-projects',
+  templateUrl: './latest-projects.component.html',
+  styleUrls: ['./latest-projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit, OnDestroy {
+export class LatestProjectsComponent implements OnInit, OnDestroy {
   subscriptions = new Subscription()
   displayedColumns = ['createDate', 'title', 'coordinator', 'organization']
   constructor(public contentService: ContentService) {}
