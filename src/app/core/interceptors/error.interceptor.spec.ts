2dae432d24ec9fd8c340745e3d6d18307359440e
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2022-08-23 09:05:24 +0300

    fix

--- src/app/core/interceptors/error.interceptor.spec.ts
+++ src/app/core/interceptors/error.interceptor.spec.ts
@@ -5,10 +5,6 @@ describe('ErrorInterceptor', () => {
   let errorInterceptor
   let authenticationServiceSpy
 
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const Jasmine = require('jasmine')
-  const jasmine = new Jasmine()
-
   beforeEach(() => {
     authenticationServiceSpy = jasmine.createSpyObj('AuthenticationService', ['logout'])
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
