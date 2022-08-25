bbb53ba5ab846e3334bac48e761f998bd729f38e
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2022-08-25 08:52:45 +0300

    fix

--- src/app/core/interceptors/error.interceptor.spec.ts
+++ src/app/core/interceptors/error.interceptor.spec.ts
@@ -10,7 +10,7 @@ describe('ErrorInterceptor', () => {
   let errorInterceptor: ErrorInterceptor
 
   const authService = {
-    logOut: () => {},
+    logout: () => {},
   } as unknown as AuthService
 
   beforeEach(() => {
@@ -35,5 +35,22 @@ describe('ErrorInterceptor', () => {
 
   it('should be created', () => {
     expect(errorInterceptor).toBeTruthy()
+  })
+
+  describe('When the Backend returns 401: Unauthorized', () => {
+    it('should logout the user', inject(
+      [HttpClient, HttpTestingController, AuthService],
+      (http: HttpClient, httpMock: HttpTestingController, injectedAuthService: AuthService) => {
+        const mockErrorResponse = { status: 401, statusText: 'Unauthorized' }
+        const data = 'Unauthorized'
+
+        jest.spyOn(injectedAuthService, 'logout')
+
+        http.get('/data').subscribe()
+
+        httpMock.expectOne('/data').flush(data, mockErrorResponse)
+        expect(injectedAuthService.logout).toHaveBeenCalled()
+      }
+    ))
   })
 })
