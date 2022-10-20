de2a514ff1a02a999ee4933abe1aa7364cde6221
Sandor Ban <zorckmaster@gmail.com>
2022-10-20 11:35:35 +0300

    Add some more coverage

--- src/app/shared/pipes/object-to-array.pipe.spec.ts
+++ src/app/shared/pipes/object-to-array.pipe.spec.ts
@@ -9,4 +9,10 @@ describe('ObjectToArrayPipe', () => {
   it('create an instance', () => {
     expect(pipe).toBeTruthy()
   })
+
+  it('should convert Object to Array', () => {
+    const input = {}
+    const result = pipe.transform(input)
+    expect(result).toEqual([])
+  })
 })
