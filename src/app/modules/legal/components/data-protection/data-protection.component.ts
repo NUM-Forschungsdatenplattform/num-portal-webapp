9ddcb08e08b45430ecc5b558ab38eecce1459159
Sandor Ban <sandor.ban.ext@vitagroup.ag>
2023-04-28 10:12:47 +0300

    small optimization

--- src/app/modules/legal/components/data-protection/data-protection.component.ts
+++ src/app/modules/legal/components/data-protection/data-protection.component.ts
@@ -32,6 +32,16 @@ export class DataProtectionComponent implements OnInit, OnDestroy {
   decisionList: string[]
 
   ngOnInit(): void {
+    this.getTranslations()
+
+    this.subscriptions.add(
+      this.translateService.onLangChange.subscribe((newLang) => {
+        this.getTranslations()
+      })
+    )
+  }
+
+  getTranslations(): void {
     this.generalDataList = this.translateService?.instant('DATA_PROTECTION.GENERAL_DATA_LIST')
     this.registrationList = this.translateService?.instant('DATA_PROTECTION.REGISTRATION_LIST')
     this.cookiesList = this.translateService?.instant('DATA_PROTECTION.COOKIES_LIST')
@@ -40,21 +50,6 @@ export class DataProtectionComponent implements OnInit, OnDestroy {
     )
     this.rightsList = this.translateService?.instant('DATA_PROTECTION.RIGHTS.RIGHTS_LIST')
     this.decisionList = this.translateService?.instant('DATA_PROTECTION.RIGHTS.RIGHTS_PART_J.LIST')
-
-    this.subscriptions.add(
-      this.translateService.onLangChange.subscribe((newLang) => {
-        this.generalDataList = this.translateService?.instant('DATA_PROTECTION.GENERAL_DATA_LIST')
-        this.registrationList = this.translateService?.instant('DATA_PROTECTION.REGISTRATION_LIST')
-        this.cookiesList = this.translateService?.instant('DATA_PROTECTION.COOKIES_LIST')
-        this.recipientsList = this.translateService?.instant(
-          'DATA_PROTECTION.RECIPIENTS.PART_B.RECIPIENTS_LIST'
-        )
-        this.rightsList = this.translateService?.instant('DATA_PROTECTION.RIGHTS.RIGHTS_LIST')
-        this.decisionList = this.translateService?.instant(
-          'DATA_PROTECTION.RIGHTS.RIGHTS_PART_J.LIST'
-        )
-      })
-    )
   }
 
   ngOnDestroy(): void {
