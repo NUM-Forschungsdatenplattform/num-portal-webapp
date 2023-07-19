ade513580aa266780091837d431bd377a511cd0d
Dirk Rossbach <dirk.rossbach@vitagroup.ag>
2023-07-19 16:42:55 +0200

    make the filters work

--- src/app/modules/projects/components/dialog-add-researchers/dialog-add-researchers.component.ts
+++ src/app/modules/projects/components/dialog-add-researchers/dialog-add-researchers.component.ts
@@ -31,9 +31,9 @@ import { IUserFilter } from 'src/app/shared/models/user/user-filter.interface'
 import { IGenericDialog } from 'src/app/shared/models/generic-dialog.interface'
 import { MatTableDataSource } from '@angular/material/table'
 import { MatPaginator } from '@angular/material/paginator'
-import { Sort } from '@angular/material/sort'
 import { MAT_DIALOG_DATA } from '@angular/material/dialog'
 import { DialogConfig } from '../../../../shared/models/dialog/dialog-config.interface'
+import { forEach } from 'lodash'
 
 @Component({
   templateUrl: './dialog-add-researchers.component.html',
@@ -122,10 +122,15 @@ export class DialogAddResearchersComponent implements OnInit, OnDestroy, IGeneri
   }
 
   handleFilterChange(noGet = false): void {
-    if (this.filterConfig.filterItem[0].isSelected) {
-      this.filters.type = null
-    } else {
+    if (this.filterConfig.filterItem[1].isSelected) {
       this.filters.type = 'ORGANIZATION'
+    } else {
+      this.filters.type = null
+    }
+    if (this.filterConfig.filterItem[3].isSelected) {
+      this.filters.approved = false
+    } else {
+      this.filters.approved = true
     }
 
     if (!noGet) {
