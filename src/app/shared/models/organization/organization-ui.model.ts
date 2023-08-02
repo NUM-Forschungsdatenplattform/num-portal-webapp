fbf8cc70614f7c9c8a371ee75937d4ac2bd8d716
Dirk Rossbach <dirk.rossbach@vitagroup.ag>
2023-08-02 16:46:59 +0200

    fix a problem with ogranizations that were always displayed as active

--- src/app/shared/models/organization/organization-ui.model.ts
+++ src/app/shared/models/organization/organization-ui.model.ts
@@ -20,13 +20,17 @@ export class OrganizationUiModel {
   id: number | null
   name?: string
   mailDomains: string[]
-  active: boolean
+  active?: boolean
 
   constructor(organizationApi?: IOrganization) {
     this.id = organizationApi?.id || null
     this.name = organizationApi?.name
     this.mailDomains = organizationApi?.mailDomains || []
-    this.active = organizationApi?.active || true
+    if (organizationApi?.active == null) {
+      this.active = true
+    } else {
+      this.active = organizationApi?.active
+    }
   }
 
   convertToApi(fillIn?: Partial<IOrganization>): IOrganization {
