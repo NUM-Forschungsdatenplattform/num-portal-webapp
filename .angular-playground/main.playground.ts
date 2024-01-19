c3a8bb50886fce3d27bf855d63d4fbfd666c1e51
Axel Siebert <axel.siebert@vitagroup.ag>
2024-01-19 14:27:40 +0100

    chore(NUM-2306): update angular-playground

--- .angular-playground/main.playground.ts
+++ .angular-playground/main.playground.ts
@@ -18,15 +18,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { BrowserModule } from '@angular/platform-browser'
 import { Component, NgModule } from '@angular/core'
 import { PlaygroundModule } from 'angular-playground'
-import { LayoutModule } from './app/layout/layout.module'
-import { SharedModule } from './app/shared/shared.module'
+import { LayoutModule } from '../src/app/layout/layout.module'
+import { SharedModule } from '../src/app/shared/shared.module'
 import { RouterTestingModule } from '@angular/router/testing'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { SandboxesDefined } from './sandboxes'
 
 PlaygroundModule.configure({
   selector: 'num-root',
   overlay: false,
   modules: [LayoutModule, SharedModule, RouterTestingModule, BrowserAnimationsModule],
+  sandboxesDefined: SandboxesDefined,
 })
 
 @Component({
