73e4fa932184708fc29d5ba1dc9918fb9b3c93a9
Axel Siebert <axel.siebert@vitagroup.ag>
2024-03-22 11:50:56 +0100

    feat(NUM-2294): add feedback notification for upload

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -22,8 +22,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           this.profileService.setUnapproveUser(true)
         }
 
-        const error = err.error.message || err.statusText
-        return throwError(error)
+        return throwError(() => err)
       })
     )
   }
