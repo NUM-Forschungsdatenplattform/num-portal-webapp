d19fbff2d88f11a4b5ac509e4e57bdc57409fe6a
rmueller <rabea.mueller@sva.de>
2024-04-09 15:20:10 +0200

    remove copyright claim on every class from the vitagroup

--- README.md
+++ README.md
@@ -122,7 +122,7 @@ msgstr ""
 
 ## License
 
-Copyright 2021 vitagroup AG
+Copyright 2024 Highmed e.V.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- src/app/layout/components/side-menu/constants.ts
+++ src/app/layout/components/side-menu/constants.ts
@@ -1,19 +1,3 @@
-/**
- * Copyright 2021 Vitagroup AG
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { DialogConfirmationComponent } from 'src/app/shared/components/dialog-confirmation/dialog-confirmation.component'
 import { DialogConfig } from 'src/app/shared/models/dialog/dialog-config.interface'
 import { DialogSize } from 'src/app/shared/models/dialog/dialog-size.enum'
