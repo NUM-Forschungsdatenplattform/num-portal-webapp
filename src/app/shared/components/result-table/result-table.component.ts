f7ea386e8744fae176a177f8a8422507234365db
Andreas Kling <andreas.kling@bioquant.uni-heidelberg.de>
2024-07-23 12:00:00 +0200

    New page "Search with AQL"

--- src/app/shared/components/result-table/result-table.component.ts
+++ src/app/shared/components/result-table/result-table.component.ts
@@ -44,13 +44,10 @@ export class ResultTableComponent {
       path: ' ',
       name: '#',
     }
-    const columnNamePattern = new RegExp('([^/]+$)')
     const resultSetColumns: IAqlExecutionColumn[] = [firstColumn, ...this.resultSet.columns]
 
     this.displayedColumns = resultSetColumns.map((column) => column.path)
-    this.displayedColumnNames = resultSetColumns.map(
-      (column) => column.name.match(columnNamePattern)[0]
-    )
+    this.displayedColumnNames = resultSetColumns.map((column) => column.name)
     this.dataSource.data = this.resultSet.rows.map((row, index) => [index + 1, ...row])
   }
 }
