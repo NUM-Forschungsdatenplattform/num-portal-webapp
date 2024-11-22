f25f4e907abce410ad244deb4870fb053d488d8f
rabeaM <rabea.mueller@sva.de>
2024-11-22 15:06:07 +0100

    test datafilter fix

--- src/app/modules/search/components/data-filter/data-filter.component.spec.ts
+++ src/app/modules/search/components/data-filter/data-filter.component.spec.ts
@@ -19,6 +19,7 @@ import { mockProject1 } from 'src/mocks/data-mocks/project.mock'
 import { mockCohort1 } from 'src/mocks/data-mocks/cohorts.mock'
 import { FeatureService } from '../../../../core/services/feature/feature.service'
 import { HttpClient, HttpHandler } from '@angular/common/http'
+import { AppConfigService } from '../../../../config/app-config.service'
 
 describe('DataFilterComponent', () => {
   let component: DataFilterComponent
@@ -32,6 +33,18 @@ describe('DataFilterComponent', () => {
     @Input() totalCohortSize: number
     @Output() determineHits = new EventEmitter()
   }
+
+  const mockConfigService = {
+    config: {
+      api: {
+        baseUrl: '/api',
+      },
+      welcomePageTitle: {
+        de: 'Test Seite',
+        en: 'Test page',
+      },
+    },
+  } as AppConfigService
 
   const mockPatientFilterService = {
     setCurrentProject: jest.fn(),
@@ -83,6 +96,10 @@ describe('DataFilterComponent', () => {
         {
           provide: CohortService,
           useValue: mockCohortService,
+        },
+        {
+          provide: AppConfigService,
+          useValue: mockConfigService,
         },
         {
           provide: AuthService,
