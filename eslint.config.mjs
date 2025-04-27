b793f126929cac95f74caf235b6fd87a97203dac
pprinzSVA <philip.prinz@sva.de>
2025-04-27 18:54:39 +0200

    update outdated dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/dist", "**/coverage"]), {
+    languageOptions: {
+        ecmaVersion: 2020,
+        sourceType: "script",
+    },
+}, {
+    files: ["**/*.ts"],
+
+    extends: compat.extends(
+        "plugin:@angular-eslint/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    languageOptions: {
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["tsconfig.json"],
+            createDefaultProgram: true,
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-non-null-assertion": "warn",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@angular-eslint/component-class-suffix": "off",
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+        }],
+    },
+}, {
+    files: ["**/*.html"],
+
+    extends: compat.extends(
+        "plugin:@angular-eslint/template/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    rules: {},
+}]);
